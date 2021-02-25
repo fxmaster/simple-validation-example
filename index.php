c29e1bac05d730ed9dcc1ebcfd653278bb501cfa
Red-373 <leo@leopart.kz>
2021-02-25 16:34:29 +0600

    Changed index.php

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Index page of task 2</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<meta charset="UTF-8">
+<title>Index page of task 2</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
 <body>
 <div class="container mt-5">
 
-	<form class="col-md-5" action="controllers/DefaultController.php" method="POST">
-		<div class="form-group">
-			<label for="username">Username</label>
-			<input name="username" type="text" class="form-control" id="username" aria-describedby="usernameHelp" placeholder="Enter username">
-		</div>
-		<div class="form-group">
-			<label for="email">Email address</label>
-			<input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-			<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-		</div>
-		<div class="form-group">
-			<label for="password1">Password</label>
-			<input type="password" class="form-control" name="password1" id="password1" placeholder="Password">
-		</div>
-		<div class="form-group">
-			<label for="password2">Confirm password</label>
-			<input type="password" class="form-control" name="password2" id="password2" placeholder="Password">
-		</div>
-		<button type="submit" class="btn btn-primary">Submit</button>
-	</form>
+<form class="col-md-5" action="controllers/DefaultController.php" method="POST">
+    <div class="form-group">
+        <label for="username">Username</label>
+        <input name="username" type="text" class="form-control" id="username" aria-describedby="usernameHelp" placeholder="Enter username">
+    </div>
+    <div class="form-group">
+        <label for="email">Email address</label>
+        <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label for="password1">Password</label>
+        <input type="password" class="form-control" name="password1" id="password1" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label for="password2">Confirm password</label>
+        <input type="password" class="form-control" name="password2" id="password2" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 
 </div>
 
